8f9d9a3782dbfd9e35a464aa073d0cbc10b403f8
marcelleslowery <marcelleslowery@MLOWERY-MBP-ATL.local>
2019-01-30 17:10:38 -0600

    adding edit page

--- src/edit.js
+++ src/edit.js
@@ -3,42 +3,18 @@ let archCat2 = ['Problem Solving', 'Frameworks & Models', 'Thought Leadership'];
 let comCat3 = ['Writing', 'Speaking', 'Meetings', 'Listening', 'Expression', 'Effective use of time'];
 let frameCat3 = ['Using Frameworks', 'Systems Thinking', 'Feedback Loops', 'Design Principles'];
 
-let predictiveList = comCat3.concat(frameCat3);
-let list = document.getElementById('cdpList');
-
-predictiveList.forEach(function(item){
-   let option = document.createElement('option');
-   option.value = item;
-   list.appendChild(option);
-});
-
-let expUl = document.getElementById('cat2');
-let catUl = document.getElementById('cat3');
+let expUl = document.getElementById('edit2');
+let catUl = document.getElementById('edit3');
 let curCdp = {
-  role: document.getElementById('cat1-btn').value,
-  exp: document.getElementById('cat2-btn').value,
-  cat: document.getElementById('cat3-btn').value,
+  role: document.getElementById('edit1-btn').value,
+  exp: document.getElementById('edit2-btn').value,
+  cat: document.getElementById('edit3-btn').value,
   set() {
-    this.role=document.getElementById('cat1-btn').value;
-    this.exp=document.getElementById('cat2-btn').value;
-    this.cat=document.getElementById('cat3-btn').value;
+    this.role=document.getElementById('edit1-btn').value;
+    this.exp=document.getElementById('edit2-btn').value;
+    this.cat=document.getElementById('edit3-btn').value;
   }
 };
-
-let findFins = () => {
-  let cdpToSearch = Object.create(curCdp);
-  cdpToSearch.set();
-  console.log(cdpToSearch);
-  //find fins that meet filter reqs
-}
-
-let findFinsFromSearch = () => {
-  let cdpToSearch = Object.create(curCdp);
-  let val = document.getElementById('searchbar').value;
-  if(val){
-    console.log(comCat3.includes(val));
-  }
-}
 
 $("ul").on("click", "li", function() {
   let selText = $(this).text();
@@ -47,12 +23,10 @@ $("ul").on("click", "li", function() {
   filter(selText);
 });
 
-
-
 let filter = (val) => {
   if(val === "Professional") {//lvl2
     resetChildren(1);
-    document.getElementById('cat2-btn').disabled = false;
+    document.getElementById('edit2-btn').disabled = false;
     profCat2.forEach(function(item) {
       let li = document.createElement('li');
       li.textContent = item;
@@ -62,7 +36,7 @@ let filter = (val) => {
     })
   } else if (val === "Architect") {//lvl2
     resetChildren(1);
-    document.getElementById('cat2-btn').disabled = false;
+    document.getElementById('edit2-btn').disabled = false;
     archCat2.forEach(function(item) {
       let li = document.createElement('li');
       li.textContent = item;
@@ -72,7 +46,7 @@ let filter = (val) => {
     })
   } else if (val === "Communication") {//lvl3
     resetChildren(2);
-    document.getElementById('cat3-btn').disabled = false;
+    document.getElementById('edit3-btn').disabled = false;
     comCat3.forEach(function(item) {
       let li = document.createElement('li');
       li.textContent = item;
@@ -82,7 +56,7 @@ let filter = (val) => {
     })
   } else if (val === "Frameworks & Models") {//lvl3
     resetChildren(2);
-    document.getElementById('cat3-btn').disabled = false;
+    document.getElementById('edit3-btn').disabled = false;
     frameCat3.forEach(function(item) {
       let li = document.createElement('li');
       li.textContent = item;
@@ -90,6 +64,8 @@ let filter = (val) => {
       li.value = item;
       catUl.appendChild(li);
     })
+  } else if(comCat3.includes(val) || frameCat3.includes(val)) {
+
   }
 }
 
@@ -104,19 +80,25 @@ let resetChildren = (lvl) => {
   if (lvl == 1) {
     clear(expUl);
     clear(catUl);
-    document.getElementById('cat2-btn').disabled = true;
-    document.getElementById('cat2-btn').innerHTML = "Select an Expectation";
-    document.getElementById('cat2-btn').value = "";
-    document.getElementById('cat3-btn').disabled = true;
-    document.getElementById('cat3-btn').innerHTML = "Select a Category";
-    document.getElementById('cat3-btn').value = "";
-
-    //minimum requirement to be able to fitler is met
-    document.getElementById('filter-btn').disabled = false;
+    document.getElementById('edit2-btn').disabled = true;
+    document.getElementById('edit2-btn').innerHTML = "Select an Expectation";
+    document.getElementById('edit2-btn').value = "";
+    document.getElementById('edit3-btn').disabled = true;
+    document.getElementById('edit3-btn').innerHTML = "Select a Category";
+    document.getElementById('edit3-btn').value = "";
   } else if (lvl == 2) {
     clear(catUl);
-    document.getElementById('cat3-btn').disabled = true;
-    document.getElementById('cat3-btn').innerHTML = "Select a Category";
-    document.getElementById('cat3-btn').value = "";
+    document.getElementById('edit3-btn').disabled = true;
+    document.getElementById('edit3-btn').innerHTML = "Select a Category";
+    document.getElementById('edit3-btn').value = "";
+  }
+}
+
+let save = () => {
+  console.log(document.getElementById('comment').value)
+  if(document.getElementById('edit3-btn').value == "" || document.getElementById('comment').value == "") {
+    alert("Please input all fields for your CDP before saving!")
+  } else {
+    //save CDPs
   }
 }
